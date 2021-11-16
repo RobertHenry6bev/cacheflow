f42d7e746f8bbdebbc66965c95f5d6e6e2d6bd41
Ubuntu <robhenry@microsoft.com>
2021-11-16 19:49:57 +0000

    capture WIP pthreading the flood code to track eor

--- experiments/e11_flood.c
+++ experiments/e11_flood.c
@@ -2,18 +2,42 @@
 // Flood the icache with straight line code.
 //
 #include <inttypes.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 #include "params.h"
 
 int foo(int i) {
-  asm(".rept 4096\neor w0, w0, 2\n.endr\n");
+  int iterations;
+  for (iterations = 0; iterations < 1000*200; iterations++) {
+    asm(".rept 4096\neor w0, w0, 2\n.endr\n");  // 0x521f0000
+  }
   return i;
 }
 
+void *foo_runner(void *vp) {
+  int i = foo(*(int *)vp);
+  (void)i;
+  return NULL;
+}
+
+#define NTHREAD 4
 int main() {
-  int iterations;
-  for (iterations = 0; iterations < 1000*1000*200; iterations++) {
-      foo(iterations);
+  int i;
+  int thread_arg[NTHREAD];
+  pthread_t *worker_thread = calloc(NTHREAD, sizeof(pthread_t));
+  for (i = 0; i < NTHREAD; i++) {
+    thread_arg[i] = i;
+    pthread_create(
+      &worker_thread[i],
+      NULL,
+      foo_runner,
+      &thread_arg[i]);
+  }
+  sleep(10);
+  for (i = 0; i < NTHREAD; i++) {
+    pthread_join(worker_thread[i], NULL);
   }
   return 0;
 }
