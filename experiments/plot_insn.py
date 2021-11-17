dbfa34cefee96f7021641a35b27836a972a0dbce
Ubuntu <robhenry@microsoft.com>
2021-11-17 20:26:42 +0000

    capture WIP mp4 movies

--- experiments/plot_insn.py
+++ experiments/plot_insn.py
@@ -17,32 +17,53 @@ def plot_insn_bitmap():
         type=int,
         default=0x521f0000,)
     parser.add_argument(
+        "--maxrows",
+        help="maximum number of rowsd",
+        type=int,
+        default=32,)
+    parser.add_argument(
+        "--output",
+        help="name of output file",
+        type=str,
+        default="junk.png",)
+    parser.add_argument(
         "rest",
         nargs=argparse.REMAINDER,)
 
     args = parser.parse_args()
 
-    with open("junk.png", "wb") as png_file:
-        png_writer = png.Writer(16, 3*256, greyscale=True)
-
+    with open(args.output, "wb") as png_file:
+        png_writer = png.Writer(16, args.maxrows, greyscale=False)
         fieldnames = ["a", "b"] + ["d_%02d" % (i,) for i in range(0, 16)]
         for input_file_name in args.rest:
             with open(input_file_name, "r") as input_fd:
-                print("open file %s" % (input_file_name,))
                 reader = csv.DictReader(input_fd, fieldnames=fieldnames)
+                nrows = 0
                 png_matrix = []
                 for row in reader:
+                    if nrows >= args.maxrows:
+                        break
                     png_row = []
                     for i in range(0, 16):
-                        field = row["d_%02d" % (i,)]
-                        # print(" %s" % (field,))
-                        given = int(field, 16)
-                        expect = args.insn
-                        if given == expect:
-                            png_row.append(0xff)
+                        field_value = int(row["d_%02d" % (i,)], 16)
+                        search_value = args.insn
+                        do_bw_match = False
+                        if do_bw_match:
+                            if field_value == search_value:
+                                png_row.append(0xff)
+                                png_row.append(0xff)
+                                png_row.append(0xff)
+                            else:
+                                png_row.append(0x00)
+                                png_row.append(0x00)
+                                png_row.append(0x00)
                         else:
-                            png_row.append(0x00)
+                            png_row.append((field_value>>(0*8)) & 0xFF)
+                            png_row.append((field_value>>(1*8)) & 0xFF)
+                            png_row.append((field_value>>(2*8)) & 0xFF)
+
                     png_matrix.append(png_row)
+                    nrows += 1
             png_writer.write(png_file, png_matrix)
 
 if __name__ == "__main__":
