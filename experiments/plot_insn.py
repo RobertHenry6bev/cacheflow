d2f8e2915df802716b6ea408401d749e079a5489
Ubuntu <robhenry@microsoft.com>
2021-11-17 19:19:15 +0000

    png plot of special instructions

--- experiments/plot_insn.py
+++ experiments/plot_insn.py
@@ -2,17 +2,14 @@
 
 """
 Write a png file which highlights where spoecific instructions are.
-
 """
-# pylint: disable=chained-comparison
-
-
 import argparse
 import csv
-import re
-import time
 
-def run_combiner():
+import png  # sudo apt-get install python3-pip ; pip3 install pypng
+
+def plot_insn_bitmap():
+    """Plot the L1 Cache as a bitmap."""
     parser = argparse.ArgumentParser("write png file showing location of instructions")
     parser.add_argument(
         "--insn",
@@ -24,24 +21,29 @@ def run_combiner():
         nargs=argparse.REMAINDER,)
 
     args = parser.parse_args()
-    fieldnames = ["a", "b"] + ["d_%02d" % (i,) for i in range(0, 16)]
-    for input_file_name in args.rest:
-        with open(input_file_name, "r") as input_fd:
-           print("open file %s" % (input_file_name,))
-           reader = csv.DictReader(input_fd, fieldnames=fieldnames)
-           for row in reader:
-               for i in range(0, 16):
-                   field = row["d_%02d" % (i,)]
-                   # print(" %s" % (field,))
-                   given = int(field, 16)
-                   expect = args.insn
-                   # print("%s %s 0x%08x vs 0x%08x\n" % (type(given), type(expect), given, expect,))
-                   if given == expect:
-                       print("1", end="")
-                   else:
-                       print(" ", end="")
-               print("")
+
+    with open("junk.png", "wb") as png_file:
+        png_writer = png.Writer(16, 3*256, greyscale=True)
+
+        fieldnames = ["a", "b"] + ["d_%02d" % (i,) for i in range(0, 16)]
+        for input_file_name in args.rest:
+            with open(input_file_name, "r") as input_fd:
+                print("open file %s" % (input_file_name,))
+                reader = csv.DictReader(input_fd, fieldnames=fieldnames)
+                png_matrix = []
+                for row in reader:
+                    png_row = []
+                    for i in range(0, 16):
+                        field = row["d_%02d" % (i,)]
+                        # print(" %s" % (field,))
+                        given = int(field, 16)
+                        expect = args.insn
+                        if given == expect:
+                            png_row.append(0xff)
+                        else:
+                            png_row.append(0x00)
+                    png_matrix.append(png_row)
+            png_writer.write(png_file, png_matrix)
 
 if __name__ == "__main__":
-    run_combiner()
-
+    plot_insn_bitmap()
