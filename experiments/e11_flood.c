6d88d82a848e8e5e9d8346e69e8667816880b697
Ubuntu <robhenry@microsoft.com>
2021-11-25 04:35:30 +0000

    more fiddle with JIT

--- experiments/e11_flood.c
+++ experiments/e11_flood.c
@@ -12,32 +12,11 @@
 
 #include "params.h"
 
-#include "flooders.c.out"
-
-typedef int(*foorunner)(int);
-static foorunner foorunners[] = {
-  foo_0x0,
-  foo_0x1,
-  foo_0x2,
-  foo_0x3,
-  foo_0x4,
-  foo_0x5,
-  foo_0x6,
-  foo_0x7,
-  foo_0x8,
-  foo_0x9,
-  foo_0xa,
-  foo_0xb,
-  foo_0xc,
-  foo_0xd,
-  foo_0xe,
-  foo_0xf,
-};
+typedef int(*fooworker)(int);
 
 void *foo_runner(void *vp) {
-  int runner_number = getpid() % 16;
-  printf("runner_number=%d\n", runner_number);
-  int i = foorunners[runner_number](*(int *)vp);
+  fooworker func = *(fooworker*)vp;
+  int i = func(1);  // argument is ignored
   (void)i;
   return NULL;
 }
@@ -56,7 +35,7 @@ uint32_t *get_aligned_code(size_t pagesize, size_t npages) {
       memptr,
       npages * pagesize,
       PROT_READ|PROT_WRITE|PROT_EXEC) != 0) {
-    perror("mprotect");
+    perror("mprotect to make writeable and executable");
   }
   return (uint32_t *)(memptr);
 }
@@ -65,58 +44,111 @@ uint32_t *get_aligned_code(size_t pagesize, size_t npages) {
 #define BRANCH_PLUS_4 0x14000004
 
 uint32_t *fill_aligned_code(uint32_t *code, size_t ninsns) {
-   int i = 0;
-   pid_t pid = getpid();
-   assert(ninsns > 64);
-   code[i++] = 0x5291b001;  // movw 1, #0x8d80
-   code[i++] = 0x72a00b61;  // movk w1, #0x5b, lsl #16
-   int Ltop = i;
-   while (i % 16 != 0) {
-     code[i++] = NOP;
-   }
-   while (i < ninsns - 20) {
-     code[i++] = BRANCH_PLUS_4;
-     code[i++] = 0xffffffff;  // marker
-     code[i++] = pid;         // pid
-     uint32_t address_self = (uint32_t)(intptr_t)&code[i];  // Take that, you type system!
-     code[i++] = address_self;
+  int i = 0;
+  pid_t pid = getpid();
+  assert(ninsns >= 32);
+
+  //
+  // for a loop iteration of 6M times:
+  // 6000000 == 6 * 1000 * 1000 == (0x5b<<16) + 0x8d80
+  //
+  {
+    code[i++] = 0x5291b001;  // movw w1, #0x8d80
+    //
+    // insn[20:5] is the value to movk
+    //
+    uint32_t movk = 0x72a00b61;  // movk w1, #0x5b, lsl #16 (base)
+    uint32_t mask = ((1 << (20-5+1)) - 1) << 5;
+    uint32_t upper = 0x08;  // originally 0x5b to get 6M
+    (void)upper; (void)mask;
+    movk = (movk & ~mask) | ((upper << 5) & mask);
+    code[i++] = movk;
   }
-  code[i++] = 0x71000421; //  subs w1, w1, #0x1
-  int deltai = Ltop - i;
-  uint32_t imm_mask = ((1<<(23-5+1)) - 1) << 5;
-  uint32_t bne = 0x54effe21; //  b.ne 0xd88 <foo_0x0+8>  // TODO FIXME Immediate field is 23:5; low 4 bits encode cond
-  bne &= ~imm_mask;
-  bne |= (deltai << 5) & imm_mask;
-  code[i++] = bne;
 
-  code[i++] = 0xd65f03c0; //  ret
+  int Ltop = i;
+  while (i % 16 != 0) {
+    code[i++] = NOP;
+  }
+  while (i < ninsns - 20) {
+    code[i++] = BRANCH_PLUS_4;
+    if (1) {
+      code[i++] = 0xffffffff;  // marker
+      code[i++] = pid;         // pid
+      uint32_t address_self = (uint32_t)(intptr_t)&code[i];  // Take that, you type system!
+      code[i++] = address_self;
+    } else {
+      code[i++] = NOP;
+      code[i++] = NOP;
+      code[i++] = NOP;
+    }
+ }
 
-  return &code[i];
+ //
+ // Generate loop test and branch back to top for more
+ //
+ code[i++] = 0x71000421; //  subs w1, w1, #0x1
+ {
+   int deltai = Ltop - i;
+   uint32_t imm_mask = ((1<<(23-5+1)) - 1) << 5;
+   uint32_t bne = 0x54effe21; //  b.ne 0xd88 // imm field is 23:5; low 4 bits encode cond
+   bne &= ~imm_mask;
+   bne |= (deltai << 5) & imm_mask;
+   code[i++] = bne;
+ }
+
+ code[i++] = 0xd65f03c0; //  ret
+
+ return &code[i];
+}
+
+void lock_aligned_code(void *memptr, size_t pagesize, size_t npages) {
+  if (mprotect(
+      memptr,
+      npages * pagesize,
+      PROT_READ|PROT_EXEC) != 0) {
+    perror("mprotect to make wrieable and executable");
+  }
 }
 
 #define NTHREAD 4  //  Raspberry Pi 4
 int main(int argc, const char **argv) {
   int i;
-  int thread_arg[NTHREAD];
+  fooworker thread_arg[NTHREAD];
   pthread_t *worker_thread = calloc(NTHREAD, sizeof(pthread_t));
 
   size_t pagesize = getpagesize();
   size_t npages = 16;
   size_t ninsns = (pagesize * npages) / sizeof(uint32_t);
+
   uint32_t *code_block = get_aligned_code(pagesize, npages);
   fill_aligned_code(code_block, ninsns);
-  foorunner func = (foorunner)code_block;
-  func(1);
+  lock_aligned_code(code_block, pagesize, npages);
+
+  __builtin___clear_cache(code_block, code_block+ninsns);  // builtin for gcc
+
+#if 0
+  asm("dsb sy");
+  asm("dmb sy");
+  asm("isb");
+  asm("isb");
+  asm("isb");
+#endif
+
+  fooworker func = (fooworker)code_block;
+  if (0) {
+    func(1);
+    printf("DONE!\n");
+    return 0;
+  }
 
   for (i = 0; i < NTHREAD; i++) {
-    thread_arg[i] = i;
+    thread_arg[i] = func;
     pthread_create(
       &worker_thread[i],
       NULL,
       foo_runner,
       &thread_arg[i]);
   }
-  // sleep(10);
   for (i = 0; i < NTHREAD; i++) {
     pthread_join(worker_thread[i], NULL);
   }
