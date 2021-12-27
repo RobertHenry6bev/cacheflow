73e4ed1a7197d28010f6078733a2b04a0566dbd5
Robert R. Henry <robhenry@microsoft.com>
2021-12-27 21:37:03 +0000

    add new.20.04

--- capture.sh
+++ capture.sh
@@ -6,11 +6,13 @@
 
 set -ux
 dstdir=baseline.20.04
+dstdir=new.20.04
 mkdir -p $dstdir
 sudo cp /proc/iomem $dstdir
      cp /boot/firmware/btcmd.txt $dstdir
      cp /boot/firmware/nobtcmd.txt $dstdir
      cp /boot/firmware/usercfg.txt $dstdir
+     cp /boot/firmware/cmdline.txt $dstdir
      cp /etc/lsb-release $dstdir
      uname -a > $dstdir/uname-a
 sudo chown $USER:$USER $dstdir/*
