663c347bef35e5294c3b8f1f27d8010a2ef24186
Robert R. Henry <robhenry@microsoft.com>
2021-12-27 18:30:10 +0000

    capture new.18.04

--- capture.sh
+++ capture.sh
@@ -5,12 +5,14 @@
 #
 
 set -ux
-dstdir=works.18.04
+dstdir=new.18.04
 mkdir -p $dstdir
 sudo cp /proc/iomem $dstdir
      cp /boot/firmware/btcmd.txt $dstdir
      cp /boot/firmware/nobtcmd.txt $dstdir
      cp /boot/firmware/usercfg.txt $dstdir
+     cp /etc/lsb-release $dstdir
+     uname -a > $dstdir/uname-a
 sudo chown $USER:$USER $dstdir/*
 sudo chmod oug-w       $dstdir/*
 
